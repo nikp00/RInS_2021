61a0c0cad642830ecfe6c24f6318a38df68778b3
nikp00 <nik.princic@gmail.com>
2021-03-12 00:31:49 +0100

    Added waypoints and markers in hw_3, added markers and camera in rviz

--- src/hw_3/scripts/simple_waypoints.py
+++ src/hw_3/scripts/simple_waypoints.py
@@ -9,8 +9,9 @@ import json
 import tf
 
 from geometry_msgs.msg import PoseStamped, Quaternion, Point
-from actionlib_msgs.msg import GoalStatusArray
 from move_base_msgs.msg import MoveBaseActionResult
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Point
 
 
 class SimpleWaypoints:
@@ -22,16 +23,15 @@ class SimpleWaypoints:
         self.result_sub = rospy.Subscriber(
             "/move_base/result", MoveBaseActionResult, self.result_sub_callback
         )
-        self.status_sub = rospy.Subscriber(
-            "/move_base/status", GoalStatusArray, self.status_sub_callback
+
+        self.marker_publisher = rospy.Publisher(
+            "/waypoint_marker", Marker, queue_size=10
         )
-        self.waypoints = iter(
-            [(i, e) for i, e in enumerate(json.load(open(waypoints_file)))]
-        )
+
+        self.waypoints = json.load(open(waypoints_file))
 
         self.waypoint_status = 3
         self.seq = 0
-        self.res_status = 0
 
         self.run()
 
@@ -41,46 +41,88 @@ class SimpleWaypoints:
             print(self.pose_publisher.get_num_connections())
             r.sleep()
 
-        while not rospy.is_shutdown() and self.res_status != -1:
+        while not rospy.is_shutdown() and self.seq < len(self.waypoints):
             if self.waypoint_status == 3:
                 self.waypoint_status = 0
                 self.send_next_waypoint()
             r.sleep()
 
     def send_next_waypoint(self):
-        self.seq, waypoint = next(self.waypoints, (-1, None))
-        if waypoint != None:
-            msg = PoseStamped()
+        waypoint = self.waypoints[self.seq]
+        self.send_marker(self.seq, waypoint)
 
-            msg.header.seq = self.seq
-            msg.header.stamp = rospy.Time.now()
-            msg.header.frame_id = "map"
+        msg = PoseStamped()
+        msg.header.seq = self.seq
+        msg.header.stamp = rospy.Time.now()
+        msg.header.frame_id = "map"
 
-            msg.pose.position = Point(*waypoint["position"])
+        msg.pose.position = Point(*waypoint["position"])
 
-            msg.pose.orientation = Quaternion(
-                *list(
-                    tf.transformations.quaternion_from_euler(
-                        0, 0, waypoint["orientation"][2]
-                    )
+        msg.pose.orientation = Quaternion(
+            *list(
+                tf.transformations.quaternion_from_euler(
+                    0, 0, waypoint["orientation"][2]
                 )
             )
+        )
 
-            self.pose_publisher.publish(msg)
-            print(msg)
-            print(self.pose_publisher.get_num_connections())
+        self.pose_publisher.publish(msg)
+        print(msg)
+        print(self.pose_publisher.get_num_connections())
 
-    def status_sub_callback(self, data):
-        pass
+    def send_marker(
+        self,
+        id,
+        waypoint,
+        sx=0.5,
+        sy=0.5,
+        r=0,
+        g=0,
+        b=0,
+        a=1,
+        mType=Marker.SPHERE,
+        action=Marker.ADD,
+        lifetime=0,
+    ):
+        msg = Marker()
+        msg.header.frame_id = "map"
+        msg.header.stamp = rospy.Time.now()
+
+        msg.ns = "waypoints"
+        msg.id = id
+        msg.type = mType
+        msg.action = action
+
+        point = Point(*waypoint["position"])
+
+        msg.pose.position = point
+        msg.scale.x = sx
+        msg.scale.y = sy
+        msg.scale.z = 0
+
+        msg.color.r = r
+        msg.color.g = g
+        msg.color.b = b
+        msg.color.a = a
+
+        msg.lifetime = rospy.Duration(lifetime)
+
+        self.marker_publisher.publish(msg)
 
     def result_sub_callback(self, data):
-        self.res_status = data.status.status
-        print(f"status: {self.res_status}")
-        if self.res_status == 3:
+        res_status = data.status.status
+        print(f"status: {res_status}")
+        if res_status == 3:
             self.waypoint_status = 3
-            if self.seq == -1:
-                self.res_status = -1
-            print("Goal reached", self.res_status)
+            self.send_marker(
+                self.seq,
+                self.waypoints[self.seq],
+                a=0.2,
+                action=Marker.MODIFY,
+            )
+
+            self.seq += 1
+            print("Goal reached", res_status)
 
 
 if __name__ == "__main__":
