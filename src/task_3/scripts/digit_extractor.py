b1949f5232b343830f9671f00c52732ff4bdac7c
nikp00 <nik.princic@gmail.com>
2021-06-01 17:49:46 +0200

    Fixed digit extractor

--- src/task_3/scripts/digit_extractor.py
+++ src/task_3/scripts/digit_extractor.py
@@ -19,35 +19,47 @@ class DigitExtractor:
         self.aruco_params = cv2.aruco.DetectorParameters_create()
         self.aruco_params.adaptiveThreshConstant = 25
         self.aruco_params.adaptiveThreshWinSizeStep = 2
+
+        self.image_publisher = rospy.Publisher("fine_navigation_image", Image, queue_size=10)
         rospy.spin()
+
+        # r = rospy.Rate(1)
+        # while not rospy.is_shutdown():
+        #     self.extract(1)
+        #     r.sleep()
 
     def extract(self, req):
         print("Start detection")
         try:
-            image = self.bridge.imgmsg_to_cv2(
+            self.image = self.bridge.imgmsg_to_cv2(
                 rospy.wait_for_message("/camera/rgb/image_raw", Image), "bgr8"
             )
         except CvBridgeError as e:
             print(e)
-        print("IMG OK")
 
-        img = image[:, : image.shape[1] // 2, :]
-        blurred = cv2.GaussianBlur(img, (0, 0), 6)
-        img = cv2.addWeighted(img, 1.5, blurred, -0.5, 0)
-        # cv2.imshow("test", img)
-        # cv2.waitKey(1)
+        image = self.find_markers()
+        if image is None:
+            return ExtractDigitsServiceResponse(0, 1)
+        res = self.extract_digits(image)
+        print(res)
+        return res
 
-        print("Image recieved")
+    def find_markers(self):
+        img = self.image[:, : self.image.shape[1] // 2, :]
+        img_aruco = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         corners, ids, rejected_corners = cv2.aruco.detectMarkers(
-            img, self.aruco_dict, parameters=self.aruco_params
+            img_aruco, self.aruco_dict, parameters=self.aruco_params
         )
+
+        if ids is None:
+            return
+
         image_size = (351, 248, 3)
         marker_side = 50
-        print("Aruco computed")
 
         img_out = np.zeros(image_size, np.uint8)
-        out_pts = np.array(
+        img_out_corners = np.array(
             [
                 [marker_side / 2, img_out.shape[0] - marker_side / 2],
                 [img_out.shape[1] - marker_side / 2, img_out.shape[0] - marker_side / 2],
@@ -57,40 +69,44 @@ class DigitExtractor:
         )
 
         src_points = np.zeros((4, 2))
-        cens_mars = np.zeros((4, 2))
+        marker_centers = np.zeros((4, 2))
 
-        if not ids is None:
-            print("IDs found")
+        center_point = []
+        for idx in ids:
+            if len(corners) > idx[0] - 1:
+                corners_centers = np.squeeze(corners[idx[0] - 1])
+                marker_center = np.mean(corners_centers, axis=0)
+                marker_centers[idx[0] - 1] = marker_center
+                center_point = np.mean(marker_centers, axis=0)
 
-            cen_point = []
-            for idx in ids:
-                if idx[0] - 1 >= len(corners):
-                    continue
-                cors = np.squeeze(corners[idx[0] - 1])
-                cen_mar = np.mean(cors, axis=0)
-                cens_mars[idx[0] - 1] = cen_mar
-                cen_point = np.mean(cens_mars, axis=0)
+        if len(center_point) == 0:
+            return ExtractDigitsServiceResponse(0, 1)
 
-            if len(cen_point) == 0:
-                return ExtractDigitsServiceResponse(0, 1)
+        missing_markers = list()
+        for i, e in enumerate(marker_centers):
+            if e[0] == 0 and e[1] == 0:
+                missing_markers.append(i)
 
-            for coords in cens_mars:
-                #  Map the correct source points
-                if coords[0] < cen_point[0] and coords[1] < cen_point[1]:
-                    src_points[2] = coords
-                elif coords[0] < cen_point[0] and coords[1] > cen_point[1]:
-                    src_points[0] = coords
-                elif coords[0] > cen_point[0] and coords[1] < cen_point[1]:
-                    src_points[3] = coords
-                else:
-                    src_points[1] = coords
+        if missing_markers != []:
+            max_x = max(marker_centers[:, 0])
+            min_x = min([e for e in marker_centers[:, 0] if e != 0])
+            max_y = max(marker_centers[:, 1])
+            min_y = min([e for e in marker_centers[:, 1] if e != 0])
 
-            print("Cords computed")
-            h, status = cv2.findHomography(src_points, out_pts)
-            img_out = cv2.warpPerspective(image, h, (img_out.shape[1], img_out.shape[0]))
+            if len(missing_markers) == 1:
+                index = missing_markers[0]
+                if not any(e[0] == min_x and e[1] == min_y for e in marker_centers):
+                    marker_centers[index] = [min_x, min_y]
+                elif not any(e[0] == min_x and e[1] == max_y for e in marker_centers):
+                    marker_centers[index] = [min_x, max_y]
+                elif not any(e[0] == max_x and e[1] == max_y for e in marker_centers):
+                    marker_centers[index] = [max_x, max_y]
+                elif not any(e[0] == max_x and e[1] == min_y for e in marker_centers):
+                    marker_centers[index] = [max_x, min_y]
 
-            img_t = cv2.copyTo(image, mask=np.ones_like(image))
-            for e in cens_mars:
+        if len(marker_centers) > 2:
+            img_t = cv2.copyTo(self.image, mask=np.ones_like(self.image))
+            for i, e in enumerate(marker_centers):
                 img_t = cv2.rectangle(
                     img_t,
                     (int(e[0]) - 10, int(e[1]) - 10),
@@ -98,51 +114,59 @@ class DigitExtractor:
                     (255, 0, 0),
                     thickness=2,
                 )
-            # cv2.imshow("2image", img_t)
-            # cv2.waitKey(1)
+                img_t = cv2.putText(
+                    img_t,
+                    str(i),
+                    (int(e[0]) - 10, int(e[1]) - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    2,
+                    (0, 0, 255),
+                )
 
-            ################################################
-            #### Extraction of digits starts here
-            ################################################
+            ros_img = self.bridge.cv2_to_imgmsg(img_t)
+            self.image_publisher.publish(ros_img)
 
-            # Cut out everything but the numbers
-            img_out = img_out[125:221, 50:195, :]
+            for coords in marker_centers:
+                #  Map the correct source points
+                if coords[0] < center_point[0] and coords[1] < center_point[1]:
+                    src_points[2] = coords
+                elif coords[0] < center_point[0] and coords[1] > center_point[1]:
+                    src_points[0] = coords
+                elif coords[0] > center_point[0] and coords[1] < center_point[1]:
+                    src_points[3] = coords
+                else:
+                    src_points[1] = coords
 
-            # Convert the image to grayscale
-            img_out = cv2.cvtColor(img_out, cv2.COLOR_BGR2GRAY)
+            h, status = cv2.findHomography(src_points, img_out_corners)
+            img_out = cv2.warpPerspective(img_aruco, h, (img_out.shape[1], img_out.shape[0]))
+            return img_out
 
-            # Option 1 - use ordinairy threshold the image to get a black and white image
-            # ret,img_out = cv2.threshold(img_out,100,255,0)
+    def extract_digits(self, image):
+        img_out = image[125:221, 50:195]
+        # Option 1 - use ordinairy threshold the image to get a black and white image
+        # ret,img_out = cv2.threshold(img_out,100,255,0)
 
-            # Option 1 - use adaptive thresholding
-            # img_out = cv2.adaptiveThreshold(
-            #     img_out, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 5
-            # )
+        # Option 1 - use adaptive thresholding
+        # img_out = cv2.adaptiveThreshold(
+        #     img_out, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 5
+        # )
 
-            # Use Otsu's thresholding
-            ret, img_out = cv2.threshold(img_out, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        # Use Otsu's thresholding
+        ret, img_out = cv2.threshold(img_out, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-            # Pass some options to tesseract
-            config = "--psm 13 outputbase nobatch digits"
+        # Pass some options to tesseract
+        config = "--psm 13 outputbase nobatch digits"
 
-            # Visualize the image we are passing to Tesseract
-            # cv2.imshow("Warped image", img_out)
-            # cv2.waitKey(1)
-
-            text = pytesseract.image_to_string(img_out, config=config)
-            text = text.strip()
-            print("Teseract finished")
-
-            if len(text) == 2:
-                x = int(text[0])
-                y = int(text[1])
-                print("Found: ", x * 10 + y)
-                return ExtractDigitsServiceResponse(x * 10 + y, 0)
-            else:
-                print("No digit found!")
-                return ExtractDigitsServiceResponse(0, 1)
-        print("No digit found!")
-        return ExtractDigitsServiceResponse(0, 1)
+        text = pytesseract.image_to_string(img_out, config=config)
+        text = text.strip()
+        if len(text) == 2:
+            x = int(text[0])
+            y = int(text[1])
+            print("Found: ", x * 10 + y)
+            return ExtractDigitsServiceResponse(x * 10 + y, 0)
+        else:
+            print("No digit found!")
+            return ExtractDigitsServiceResponse(0, 1)
 
 
 if __name__ == "__main__":
