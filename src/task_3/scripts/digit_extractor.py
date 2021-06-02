1645573d9732937a27b266ab99a00888d970d31b
nikp00 <nik.princic@gmail.com>
2021-06-02 03:03:08 +0200

    Fixed some errors

--- src/task_3/scripts/digit_extractor.py
+++ src/task_3/scripts/digit_extractor.py
@@ -13,7 +13,6 @@ from task_3.srv import ExtractDigitsService, ExtractDigitsServiceResponse
 class DigitExtractor:
     def __init__(self):
         self.node = rospy.init_node("digit_extractor")
-        self.srv = rospy.Service("digit_extractor", ExtractDigitsService, self.extract)
 
         self.bridge = CvBridge()
 
@@ -25,9 +24,15 @@ class DigitExtractor:
         self.aruco_params.polygonalApproxAccuracyRate = 0.07
 
         self.image_publisher = rospy.Publisher("fine_navigation_image", Image, queue_size=10)
-        self.image_publisher123 = rospy.Publisher("fine_navigation_image_out", Image, queue_size=10)
+        self.image_publisher123 = rospy.Publisher("ring_image", Image, queue_size=10)
 
-        rospy.spin()
+        # self.srv = rospy.Service("digit_extractor", ExtractDigitsService, self.extract)
+        # rospy.spin()
+
+        r = rospy.Rate(1)
+        while not rospy.is_shutdown():
+            self.extract(1)
+            r.sleep()
 
     def extract(self, req):
         try:
@@ -35,23 +40,26 @@ class DigitExtractor:
                 rospy.wait_for_message("/camera/rgb/image_raw", Image), "bgr8"
             )
 
+            color_img = cv_image.copy()
             height, width, channels = cv_image.shape
 
-            cv_image = cv2.cvtColor(cv_image, cv2.COLOR_RGB2GRAY)
-            cv_image = cv2.cvtColor(cv_image, cv2.COLOR_GRAY2RGB)
+            # cv_image = cv2.cvtColor(cv_image, cv2.COLOR_RGB2GRAY)
+            # cv_image = cv2.cvtColor(cv_image, cv2.COLOR_GRAY2RGB)
+            cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+            # BLURING THE IMAGE
+            # blur = cv2.GaussianBlur(cv_image, (0, 0), 6)
+            # cv_image = cv2.addWeighted(cv_image, 1.5, blur, -0.5, 0)
 
-            #BLURING THE IMAGE
-            blur = cv2.GaussianBlur(cv_image, (0, 0), 6)
-            cv_image = cv2.addWeighted(cv_image, 1.5, blur, -0.5, 0)
-
-            #TRESHOLDING
-            ret,cv_image = cv2.threshold(cv_image,15,190,cv2.THRESH_BINARY)
-
+            # TRESHOLDING
+            # ret, cv_image = cv2.threshold(cv_image, 15, 190, cv2.THRESH_BINARY)
+            ret, cv_image = cv2.threshold(cv_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
             cropped_image = cv_image.copy()
 
-            #ARUCO MARKER DETECTION
-            corners, ids, rejected_corners = cv2.aruco.detectMarkers(cropped_image,self.aruco_dict,parameters=self.aruco_params)
+            # ARUCO MARKER DETECTION
+            corners, ids, rejected_corners = cv2.aruco.detectMarkers(
+                cropped_image, self.aruco_dict, parameters=self.aruco_params
+            )
 
             biggest_diff = 0
             prev_point = None
@@ -60,9 +68,9 @@ class DigitExtractor:
 
             # Save firsf number you found
             bestResult = None
-            cvCopy = cv_image.copy()
+            cvCopy = color_img.copy()
 
-            #Preveri vse positive cornerje
+            # Preveri vse positive cornerje
             if not corners is None:
 
                 if len(corners) >= 2:
@@ -72,36 +80,13 @@ class DigitExtractor:
                             y222 = int(cords[0][1])
 
                             x1y1 = (x111, y222)
-                            x2y2 = (x111+25, y222+25)
+                            x2y2 = (x111 + 25, y222 + 25)
 
                             all_corners.append(x1y1)
 
-                            #IZRISE KVADRAT OKOLI MARKERJA
-                            cv_image = cv2.rectangle(cropped_image,x1y1, x2y2, (190,190,190), 25)
-                            cvCopy = cv2.rectangle(cvCopy,x1y1, x2y2, (0,190,0), 5)
-
-            # Preveri vse rejectane kornerje
-            # if not rejected_corners is None:
-            #
-            #     if len(rejected_corners) >= 1:
-            #         for idx in rejected_corners:
-            #
-            #             for cords in idx:
-            #                 x111 = int(cords[0][0])
-            #                 y222 = int(cords[0][1])
-            #
-            #                 x1y1 = (x111, y222)
-            #                 x2y2 = (x111+20, y222+20)
-            #
-            #                 all_corners.append(x1y1)
-            #
-            #                 #IZRISE KVADRAT OKOLI MARKERJA
-            #                 cv_image = cv2.rectangle(cropped_image,x1y1, x2y2, (190,190,190), 25)
-            #                 cvCopy = cv2.rectangle(cvCopy,x1y1, x2y2, (0,190,0), 5)
-
-
-            # cv2.imshow('Warped image',cvCopy)
-            # cv2.waitKey(1)
+                            # IZRISE KVADRAT OKOLI MARKERJA
+                            cv_image = cv2.rectangle(cropped_image, x1y1, x2y2, (190, 190, 190), 25)
+                            cvCopy = cv2.rectangle(cvCopy, x1y1, x2y2, (0, 190, 0), 5)
 
             ros_img = self.bridge.cv2_to_imgmsg(cvCopy)
             self.image_publisher.publish(ros_img)
@@ -109,16 +94,15 @@ class DigitExtractor:
             if len(all_corners) >= 3:
                 for cords in all_corners:
                     for test in combine:
-                        if abs(test-cords[1]) <= 15:
+                        if abs(test - cords[1]) <= 15:
                             tmp = combine[test].copy()
-                            new_key = int((test + cords[1])/2)
+                            new_key = int((test + cords[1]) / 2)
                             del combine[test]
                             combine[new_key] = tmp
                             combine[new_key].append(cords)
                             break
                     else:
                         combine[cords[1]] = [cords]
-
 
             max1 = None
             keyMax1 = None
@@ -148,7 +132,7 @@ class DigitExtractor:
 
             for cords in leftCorners[keyMax2]:
                 for value in leftCorners[keyMax1]:
-                    if(abs(value[0]-cords[0]) <= 15):
+                    if abs(value[0] - cords[0]) <= 15:
                         break
                 else:
                     leftCorners[keyMax1].append((cords[0], keyMax2))
@@ -156,18 +140,18 @@ class DigitExtractor:
             leftCorners[keyMax1] = sorted(leftCorners[keyMax1])
             leftCorners[keyMax2] = sorted(leftCorners[keyMax2])
 
-            #Iskanje primernih aruco markerjev -> Gremo cez dalsi array
-            for i in range(0,len(leftCorners[keyMax1])-1):
+            # Iskanje primernih aruco markerjev -> Gremo cez dalsi array
+            for i in range(0, len(leftCorners[keyMax1]) - 1):
 
                 x1 = leftCorners[keyMax1][i][0]
-                x2 = leftCorners[keyMax1][i+1][0]
+                x2 = leftCorners[keyMax1][i + 1][0]
 
                 x1y1 = (x1, int(keyMax2))
                 x2y2 = (x2, int(keyMax1))
 
-                okayCorners.append((x1y1,x2y2))
+                okayCorners.append((x1y1, x2y2))
 
-                #Za prikaz odseka slik
+                # Za prikaz odseka slik
                 # color = list(np.random.choice(range(256), size=3))
                 # test_image = cropped_image.copy()
                 # test_image = cv2.rectangle(test_image,x1y1, x2y2, (int(color[0]),int(color[1]),int(color[2])), 5)
@@ -175,30 +159,36 @@ class DigitExtractor:
                 # cv2.waitKey(10)
                 # sleep(1)
 
-
             # Increase proportionally if you want a larger image
-            image_size=(351,248,3)
-            marker_side=50
+            image_size = (351, 248, 3)
+            marker_side = 50
 
             img_out = np.zeros(image_size, np.uint8)
-            out_pts = np.array([[marker_side/2,img_out.shape[0]-marker_side/2],
-                            [img_out.shape[1]-marker_side/2,img_out.shape[0]-marker_side/2],
-                            [marker_side/2,marker_side/2],
-                            [img_out.shape[1]-marker_side/2,marker_side/2]])
+            out_pts = np.array(
+                [
+                    [marker_side / 2, img_out.shape[0] - marker_side / 2],
+                    [img_out.shape[1] - marker_side / 2, img_out.shape[0] - marker_side / 2],
+                    [marker_side / 2, marker_side / 2],
+                    [img_out.shape[1] - marker_side / 2, marker_side / 2],
+                ]
+            )
 
-            src_points = np.zeros((4,2))
-            cens_mars = np.zeros((4,2))
-
+            src_points = np.zeros((4, 2))
+            cens_mars = np.zeros((4, 2))
 
             for cords in okayCorners:
 
                 img_out = np.zeros(image_size, np.uint8)
-                out_pts = np.array([[marker_side/2,img_out.shape[0]-marker_side/2],
-                                [img_out.shape[1]-marker_side/2,img_out.shape[0]-marker_side/2],
-                                [marker_side/2,marker_side/2],
-                                [img_out.shape[1]-marker_side/2,marker_side/2]])
+                out_pts = np.array(
+                    [
+                        [marker_side / 2, img_out.shape[0] - marker_side / 2],
+                        [img_out.shape[1] - marker_side / 2, img_out.shape[0] - marker_side / 2],
+                        [marker_side / 2, marker_side / 2],
+                        [img_out.shape[1] - marker_side / 2, marker_side / 2],
+                    ]
+                )
 
-                src_points = np.zeros((4,2))
+                src_points = np.zeros((4, 2))
                 x1 = None
                 x2 = None
                 y1 = None
@@ -222,31 +212,32 @@ class DigitExtractor:
                     y1 = cords[1][1]
                     y2 = cords[0][1]
 
-                src_points[0] = (x1,y2)
-                src_points[1] = (x2,y2)
-                src_points[2] = (x1,y1)
-                src_points[3] = (x2,y1)
+                src_points[0] = (x1, y2)
+                src_points[1] = (x2, y2)
+                src_points[2] = (x1, y1)
+                src_points[3] = (x2, y1)
 
                 h, status = cv2.findHomography(src_points, out_pts)
-                img_out = cv2.warpPerspective(cv_image, h, (img_out.shape[1],img_out.shape[0]))
+                img_out = cv2.warpPerspective(cv_image, h, (img_out.shape[1], img_out.shape[0]))
 
                 ################################################
                 ####### Extraction of digits starts here #######
                 ################################################
 
                 # Cut out everything but the numbers
-                img_out = img_out[90:290,40:300]
+                img_out = img_out[90:290, 40:300]
 
                 # Option 1 - use ordinairy threshold the image to get a black and white image
 
-                ret,img_out = cv2.threshold(img_out,100,255,0)
+                # ret, img_out = cv2.threshold(img_out, 100, 255, 0)
 
                 # Creating kernel
                 kernel = np.ones((5, 5), np.uint8)
 
                 # Using cv2.erode() method
-                img_out = cv2.erode(img_out, kernel)
+                # img_out = cv2.erode(img_out, kernel)
 
+                img_out = cv2.GaussianBlur(img_out, (5, 5), 0)
                 ros_img = self.bridge.cv2_to_imgmsg(img_out)
                 self.image_publisher123.publish(ros_img)
                 # cv2.imshow('Podobna',img_out)
@@ -254,10 +245,10 @@ class DigitExtractor:
                 # sleep(1)
 
                 # Pass some options to tesseract
-                config = '--psm 13 outputbase nobatch digits'
+                config = "--psm 13 outputbase nobatch digits"
 
                 # Visualize the image we are passing to Tesseract
-                # cv2.imshow('Warped image',img_out)
+                # cv2.imshow("Warped image", img_out)
                 # cv2.waitKey(1)
 
                 # Extract text from image
@@ -267,19 +258,21 @@ class DigitExtractor:
                 text = text.strip()
 
                 # If the extracted text is of the right length
-                if len(text)>=2:
-                    x=int(text[0])
-                    y=int(text[1])
-                    if(int(x) >= 1):
-                        bestResult = re.findall('\\d+', text)[0]
+                if len(text) >= 2:
+                    x = int(text[0])
+                    y = int(text[1])
+                    if int(x) >= 1:
+                        bestResult = re.findall("\\d+", text)[0]
+                        print(text, bestResult)
                         print("RESULT IS ", bestResult)
-                        if(int(bestResult) >= 100):
+                        if int(bestResult) >= 100:
                             return ExtractDigitsServiceResponse(0, 1)
                         else:
                             return ExtractDigitsServiceResponse(int(bestResult), 0)
             return ExtractDigitsServiceResponse(0, 1)
         except:
             return ExtractDigitsServiceResponse(0, 1)
+
 
 if __name__ == "__main__":
     DigitExtractor()
